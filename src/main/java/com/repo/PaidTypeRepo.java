1306f4716d69a27de8f24946a4b1feab8a2f6d5f
adamusmaev <adam.usmaev2000@mail.ru>
2021-04-08 18:14:46 +0300

    Add PaidTypeRepo

---
+++ src/main/java/com/repo/PaidTypeRepo.java
@@ -0,0 +1,6 @@
+package com.repo;
+import com.entities.PaidType;
+import org.springframework.data.repository.CrudRepository;
+
+public interface PaidTypeRepo extends CrudRepository<PaidType, Integer> {
+}
